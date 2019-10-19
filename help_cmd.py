a7be9a5c5394704f97fa62d07e6650d19113b7e1
lukenamop <lukenamop@gmail.com>
2019-10-18 20:42:51 -0500

    MadnessMod v1.1.21

--- help_cmd.py
+++ help_cmd.py
@@ -14,9 +14,9 @@ admin_help = """`.clearmatches` - clears all matches and votes from the database
 	\n`.clearsignups` - clears all signups from the database (**admin only**)
 	\n`.donate` - shows MadnessMod donation info
 	\n`.help` - returns this help menu
-	\n`.removetournamentroles` - remove past participants' round roles
+	\n`.removetournamentroles` - remove past participants' round roles (**admin only**)
 	\n`.resignup <user ID> <reason>` - deletes a user's template and DMs them with `<reason>`, prompting them to re-signup
-	\n`.settournamentroles` - initializes the tournament's participants' round roles (sets them to Round 1)
+	\n`.settournamentroles` - initializes the tournament's participants' round roles (sets them to Round 1) (**admin only**)
 	\n`.showresults` - shows the results of the most recent match in the match channel it's called in
 	\n`.signuplist` - displays a full list of signups for the current cycle
 	\n`.startmatch @<user> @<user>` - starts a match between two users, if an image is attached it will be supplied as their template (can only be used in a #contest channel)
