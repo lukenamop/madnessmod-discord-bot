0d603ba66f574ae490f8759b7bc40422c9fa2904
lukenamop <lukenamop@gmail.com>
2019-10-18 20:42:11 -0500

    MadnessMod v1.1.20

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ CYCLE = 604800 # 7 days
 MATCH_TIME = 1800 # 30 minutes
 MATCH_WARN1_TIME = 900 # 15 minutes
 MATCH_WARN2_TIME = 1500 # 25 minutes
-BASE_POLL_TIME = 30 # 2 hours
+BASE_POLL_TIME = 7200 # 2 hours
 ADMIN_IDS = [324273473360887808, 380462665358901268] # admin user IDs (lukenamop, eltoch)
 # round role IDs (Preliminary, Round 1, Round 2, Round 3, Semi-Finalist, Finalist)
 ROUND_ROLE_IDS = [634855120672391188, 634854136268980244, 634854070309355542, 634853838611939347, 634853782815113216, 634853736144961580]
 WINNER_ROLE_ID = 600857122032451594 # Past Winner role ID
-TESTING = True # if set to true, stats will not be recorded to the database
+TESTING = False # if set to true, stats will not be recorded to the database
 
 # verify.py stuff
 CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
