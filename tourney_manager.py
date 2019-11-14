26ed5aed127eb43a8474abe3cff44ac38ae57315
lukenamop <lukenamop@gmail.com>
2019-11-14 13:06:10 -0600

    MadnessMod v1.9.3

--- tourney_manager.py
+++ tourney_manager.py
@@ -9,6 +9,9 @@ import config
 # set challonge API credentials
 challonge.set_credentials(config.C_USERNAME, config.C_API_KEY)
 
-challonge.tournaments.create('test tournament 2', 'ttjachar88', hold_third_place_match=True, quick_advance=True)
+# challonge.tournaments.create('test tournament 2', 'ttjachar88', hold_third_place_match=True, quick_advance=True)
+challonge.participants.create('ttjachar88','lukenamop')
+challonge.participants.create('ttjachar88','eltoch')
+challonge.participants.create('ttjachar88','UncreativeFilth')
 
 # print(challonge.matches.index('mmcycle9'))
