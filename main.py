400f60d1f114873dbb9684bf0514904b405a2548
lukenamop <lukenamop@gmail.com>
2019-11-14 15:29:27 -0600

    MadnessMod v1.9.18

--- main.py
+++ main.py
@@ -1207,9 +1207,10 @@ async def on_message(message):
 		if message_content.startswith('.creatematchchannels '):
 			# check to be sure only admin user uses command
 			if message.author.id in config.ADMIN_IDS:
+				arg = message_content.split()[1]
 				mm_guild = message.guild
 				contest_category = mm_guild.get_channel(config.MATCH_CATEGORY_ID)
-				await mm_guild.create_text_channel('test', category=contest_category)
+				await mm_guild.create_text_channel(arg, category=contest_category)
 			return
 
 		# '.clearparticipantstats' command (duel-mods)
