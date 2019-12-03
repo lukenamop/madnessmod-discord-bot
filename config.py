a772e22f6d45d1288a8a5185efcd37fb10a47be3
lukenamop <lukenamop@gmail.com>
2019-12-02 21:59:00 -0600

    MadnessMod v2.2.18

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_URL = os.environ['DATABASE_URL']
 
 ##### main.py stuff
 TOKEN = os.environ['DISCORD_TOKEN']
-TESTING = False # if set to true, stats will not be recorded to the database
+TESTING = True # if set to true, stats will not be recorded to the database
 MM_GUILD_ID = 581695290986332160 # Meme Madness guild ID
 ADMIN_IDS = [324273473360887808, 380462665358901268, 583050600984346655] # admin user IDs (lukenamop, eltoch, UncreativeFilth)
 CYCLE = 1209600 # 14 days
