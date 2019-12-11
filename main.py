975b153c1db8be28071d9e203a068c09262c00fa
lukenamop <lukenamop@gmail.com>
2019-12-10 21:33:20 -0600

    MadnessMod v2.3.11

--- main.py
+++ main.py
@@ -2286,13 +2286,13 @@ async def on_reaction_add(reaction, user):
 						embed_description = f'Your vote for image {vote_position} has been confirmed. If you\'d like to change your vote, remove this vote by using the same emoji.\n\nYou have earned 10 points for voting!\nYour new voting streak is {vote_streak_string}, next streak available in 23 hours.\nYou have earned {vote_streak_bonus} bonus points for increasing your voting streak!'
 					else:
 						# calculate seconds until the user's next voting streak
-						next_streak_seconds = float((last_vote_streak_time + 82800) - time.time())
-						if round(next_streak_seconds / (60 * 60)) > 1:
+						next_streak_seconds = (last_vote_streak_time + 82800) - time.time()
+						if round(float(next_streak_seconds) / float(60 * 60)) > 1:
 							# round to the nearest hour
-							next_streak_string = f'{round(next_streak_seconds / (60 * 60))} hours'
+							next_streak_string = f'{round(float(next_streak_seconds) / float(60 * 60))} hours'
 						else:
 							# round to the nearest minute
-							next_streak_string = f'{round(next_streak_string / 60)} minutes'
+							next_streak_string = f'{round(float(next_streak_string) / float(60))} minutes'
 
 						embed_description = f'Your vote for image {vote_position} has been confirmed. If you\'d like to change your vote, remove this vote by using the same emoji.\n\nYou have earned 10 points for voting!\nYour current voting streak is {vote_streak_string}, next streak available in {next_streak_string}.'
 					embed = await generate_embed('green', embed_title, embed_description)
