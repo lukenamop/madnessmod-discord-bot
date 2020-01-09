f2988b5e59f363ddc3eaeec7207ee35d6bbbe278
lukenamop <lukenamop@gmail.com>
2020-01-08 21:49:58 -0600

    MadnessMod v2.5.14

--- functions.py
+++ functions.py
@@ -6,19 +6,19 @@ def escape_underscores(input_string):
 
 # format leaderboard entries
 def format_lb_entry(lb_page, lb_rank, member_name, lb_points):
-	if lb_page == 1:
+	if lb_page >= 1 and lb_page < 10:
 		if lb_rank < 10:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
 		elif lb_rank >= 10:
 			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
-	elif lb_page == 10:
+	elif lb_page >= 10 and lb_page < 100:
 		if lb_rank < 100:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
 		elif lb_rank >= 100:
 			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
-	elif lb_page == 100:
+	elif lb_page >= 100 and lb_page < 1000:
 		if lb_rank < 1000:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
