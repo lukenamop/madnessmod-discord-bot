7af53d93111a7bc77a653f81024b983214a2f4ca
lukenamop <lukenamop@gmail.com>
2020-01-09 11:36:32 -0600

    MadnessMod v2.6.4

--- help_cmd.py
+++ help_cmd.py
@@ -1,6 +1,7 @@
 dm_help = """`.donate` - shows MadnessMod donation info
 	\n`.help` - returns this help menu
 	\n`.signup` - submits a template to the Meme Madness moderators (must be used in the same message as an attachment)
+	\n`.stats` - displays your Meme Madness stats (can also be used like `.stats @<user>`)
 	\n`.submit` - submits a final meme to your active match
 	\n`.template` - submits a template to be used in Meme Madness matches"""
 
