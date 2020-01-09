a9ebb0accbf83ee18c2503e9f5ef5e509232f767
lukenamop <lukenamop@gmail.com>
2020-01-08 22:41:12 -0600

    MadnessMod v2.6.1

--- main.py
+++ main.py
@@ -2595,7 +2595,7 @@ async def on_reaction_add(reaction, user):
 						result = connect.crsr.fetchone()
 						if result is not None:
 							user_rank = result[0]
-							lb_page = (user_rank - (user_rank % 10)) / 10
+							lb_page = int((user_rank - (user_rank % 10)) / 10) + 1
 					elif reaction.emoji == '➡️':
 						lb_page += 1
 
