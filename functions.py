7e7477bd10da84d88582ecbb48c3a85eb40854ff
lukenamop <lukenamop@gmail.com>
2020-01-08 21:46:23 -0600

    MadnessMod v2.5.13

--- functions.py
+++ functions.py
@@ -5,24 +5,24 @@ def escape_underscores(input_string):
 	return input_string.translate(str.maketrans({'_': '\_'}))
 
 # format leaderboard entries
-def format_lb_entry(lb_page, iteration, lb_rank, member_name, lb_points):
+def format_lb_entry(lb_page, lb_rank, member_name, lb_points):
 	if lb_page == 1:
-		if iteration < 10:
+		if lb_rank < 10:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
-		elif iteration >= 10:
+		elif lb_rank >= 10:
 			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
 	elif lb_page == 10:
-		if iteration < 100:
+		if lb_rank < 100:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
-		elif iteration >= 100:
+		elif lb_rank >= 100:
 			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
 	elif lb_page == 100:
-		if iteration < 1000:
+		if lb_rank < 1000:
 			# add an extra space to align the other ranks
 			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
-		elif iteration >= 1000:
+		elif lb_rank >= 1000:
 			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
 	else:
 		return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
