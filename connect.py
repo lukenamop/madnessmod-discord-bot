b3e66a08b6767a131ccedc8ee035a4f9015fabdd
lukenamop <lukenamop@gmail.com>
2020-05-06 15:02:17 -0600

    MadnessMod v3.1.4

--- connect.py
+++ connect.py
@@ -31,9 +31,6 @@ def db_connect():
 	# crsr.execute("""UPDATE participants SET match_votes = 0, lb_points = 0, longest_vote_streak = 0, vote_streak = 0, last_vote_streak_time = 0, unvoted_match_start_time = NULL""")
 	# conn.commit()
 
-	crsr.execute("""UPDATE matches SET poll_start_time = 1588797001, poll_message_id = 707690710090121266 WHERE channel_id = 707341589814444063""")
-	conn.commit()
-
 	crsr.execute("""SELECT COUNT(*) FROM matches""")
 	result = crsr.fetchone()
 	print('matches: ' + str(result[0]))
