b65554d449fa929a2efa958745287554859643f1
lukenamop <lukenamop@gmail.com>
2020-06-14 19:50:20 -0400

    MadnessMod v3.3.1

--- main.py
+++ main.py
@@ -2036,7 +2036,7 @@ async def on_message(message):
 							member1 = last_message.mentions[0]
 							member2 = last_message.mentions[1]
 
-							await match_channel.send(f'Friendly reminder to complete this match! If you haven\'t been able to line up your availability, you should plan on doing a split match. To complete a split match, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member1.mention} {member2.mention}')
+							await match_channel.send(f'You have 24h left to complete this match! If you haven\'t been able to line up your availability, you should plan on doing a split match. To complete a split match, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member1.mention} {member2.mention}')
 							alerted += 1
 						elif len(last_message.embeds) == 1:
 							if last_message.embeds[0].title == 'Solo Match Complete':
@@ -2046,10 +2046,10 @@ async def on_message(message):
 
 								completed_member_id = last_message.embeds[0].description.split('<@')[1].split('>')[0]
 								if member1.id == completed_member_id:
-									await match_channel.send(f'Friendly reminder to complete this match! It looks like your opponent has already completed their side of a split match. To complete your half, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member2.mention}')
+									await match_channel.send(f'You have 24h left to complete this match! It looks like your opponent has already completed their side of a split match. To complete your half, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member2.mention}')
 									alerted += 1
 								elif member2.id == completed_member_id:
-									await match_channel.send(f'Friendly reminder to complete this match! It looks like your opponent has already completed their side of a split match. To complete your half, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member1.mention}')
+									await match_channel.send(f'You have 24h left to complete this match! It looks like your opponent has already completed their side of a split match. To complete your half, @ mention Duel Mods in {message.guild.get_channel(config.GENERAL_CHAN_ID).mention} when you have 30 minutes free.\n{member1.mention}')
 									alerted += 1
 								else:
 									await message.channel.send(f'{message.guild.get_member(config.ADMIN_IDS[0]).mention} there was an error reminding a member of a split match: {completed_member_id}')
