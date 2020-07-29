bc38baedacf6de5186a93783ee1ecb1cabd46ff5
lukenamop <lukenamop@gmail.com>
2020-07-28 21:10:13 -0400

    MadnessMod v4.0.20

--- config.py
+++ config.py
@@ -23,14 +23,14 @@ google_key_json['token_uri'] = os.environ['G_TOKEN_URI']
 google_key_json['auth_provider_x509_cert_url'] = os.environ['G_AUTH_PROVIDER_X509_CERT_URL']
 google_key_json['client_x509_cert_url'] = os.environ['G_CLIENT_X509_CERT_URL']
 
-outfile = open(G_KEY_FILE, 'w')
-outfile.write(json.dumps(google_key_json))
-outfile.close()
+# outfile = open(G_KEY_FILE, 'w')
+# outfile.write(json.dumps(google_key_json))
+# outfile.close()
 
 # Google API scope
 G_SCOPE = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 # Google API creds
-G_CREDS = ServiceAccountCredentials.from_json_keyfile_name(G_KEY_FILE, G_SCOPE)
+G_CREDS = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict=google_key_json, scopes=G_SCOPE)
 
 
 ##### main.py stuff #####
