94905f1a4bb7a6e4bae0895ea71de7ec55f1e232
lukenamop <lukenamop@gmail.com>
2020-08-06 11:41:07 -0400

    MadnessMod v4.6.4

--- functions.py
+++ functions.py
@@ -82,23 +82,23 @@ def format_lb_entry(lb_page, lb_rank, member_name, lb_points):
 	if lb_page >= 1 and lb_page < 10:
 		if lb_rank < 10:
 			# add an extra space to align the other ranks
-			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 		elif lb_rank >= 10:
-			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 	elif lb_page >= 10 and lb_page < 100:
 		if lb_rank < 100:
 			# add an extra space to align the other ranks
-			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 		elif lb_rank >= 100:
-			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 	elif lb_page >= 100 and lb_page < 1000:
 		if lb_rank < 1000:
 			# add an extra space to align the other ranks
-			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**` {lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 		elif lb_rank >= 1000:
-			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+			return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 	else:
-		return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points} points\n'
+		return f'**`{lb_rank}:` {escape_underscores(member_name)}** - {lb_points:,} points\n'
 
 # generate a discord embed with an optional attached image
 async def generate_embed(color, title, description, attachment=None, timestamp=None):
